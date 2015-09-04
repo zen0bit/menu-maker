92226f1651f0c0cca231cea9cf83d8881fde2195
fougas <fougas@3126ed10-1e2b-0410-ad6b-96c5846c2dce>
2015-09-04 08:53:11 +0000

    Added menu database from Debian 8.1 DVDs 1-3.

--- debian-menu-extract.sh
+++ debian-menu-extract.sh
@@ -4,14 +4,17 @@
 # rooted by command-line parameters and cat them to stdout
 # Ex:> ./debian-menu-extract.sh /var/cache/apt > menu-file
 
+shopt -s globstar
+
 for root in $*; do
-	for deb in `find $root -name "*.deb"`; do
+	for deb in $root/**/*.deb; do
+		echo `basename "$deb"` 1>&2
 		dest=`mktemp -d`
-		dpkg-deb -R $deb $dest
+		dpkg-deb -x "$deb" $dest
 		wc=$dest/usr/share/menu/*
 		for menu in `echo $wc`; do
 			if [ -f $menu ]; then
-				cat $menu | sed -e '/^#/ d' | sed -e ':a; N; s/\\[ \t]*\n//; ta' | sed -e '/^[ \t]*$/ d'
+				cat $menu | sed -e '/^[ \t]*$/ d'
 			fi
 		done
 		rm -rf $dest
