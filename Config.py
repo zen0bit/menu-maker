69716eed0ad8f77f929bb2c9e54438f4faee9371
fougas <fougas@3126ed10-1e2b-0410-ad6b-96c5846c2dce>
2017-01-10 10:56:31 +0000

    More authors fixes.

--- Config.py
+++ Config.py
@@ -1,11 +1,7 @@
-pkgHome = "http://menumaker.sourceforge.net"
-pkgName = "MenuMaker"
-pkgVer = "0.99.10"
+pkgHome		= "http://menumaker.sourceforge.net"
+pkgName		= "MenuMaker"
+pkgVer		= "0.99.10"
 
-author = "Oleg A. Khlybov"
-email = "fougas@mail.ru"
-copyright = "(c) 2002-2017"
-
-author2 = "Steve Fullenwider"
-email2 = "sfullenwider@hellburner.com"
-copyright2 = "(c) 2007-2008"
+author		= "Oleg A. Khlybov"
+email		= "fougas@mail.ru"
+copyright	= "(c) 2002-2017"
